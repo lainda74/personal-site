12c3e52ee71905b68ba8b70591d180fa022e6126
lsdinduruwage <lsdinduruwage@zuelligpharma.com>
2025-02-01 16:38:05 +0800

    Adding Transitions

--- src/components/Articles/index.tsx
+++ src/components/Articles/index.tsx
@@ -1,73 +1,116 @@
 import Image from "next/image";
 
+const articles = [
+  {
+    title: "Create Your First Angular Library — Beginner Guide",
+    description:
+      "When our projects become larger, we may have to split them into...",
+    link: "https://medium.com/@diaslalinda/create-your-first-angular-library-beginner-guide-4e0d98b13475",
+    image:
+      "https://ik.imagekit.io/dsmblx6kfve/personal-site/Npm-logo%201_DZZizb9Dx.svg?updatedAt=1738383807505",
+  },
+  {
+    title: "Micro Frontends: Introduction",
+    description:
+      "It’s not late to learn about micro frontends although the concept was first introduced in 2016...",
+    link: "https://medium.com/@diaslalinda/micro-frontends-introduction-f51b82c99032",
+    image:
+      "https://ik.imagekit.io/dsmblx6kfve/personal-site/article2_vfhRmRJ6-.svg?updatedAt=1738384157468",
+  },
+  {
+    title: "Which Text Case to Use",
+    description:
+      "Ever wondered which text case to use? A little thing like capitalization...",
+    link: "https://medium.com/@diaslalinda/which-text-case-to-use-d583b2ad2006",
+    image:
+      "https://ik.imagekit.io/dsmblx6kfve/personal-site/article3_gAFGCYQ0P.svg?updatedAt=1738384263456",
+  },
+  {
+    title: "Managing Large UI Development Projects",
+    description:
+      "Usually, people think that CSS or SASS can be written in any way...",
+    link: "https://medium.com/@diaslalinda/managing-large-ui-development-projects-61a7166834f7",
+    image:
+      "https://ik.imagekit.io/dsmblx6kfve/personal-site/article4_Ov-y-7LvB.svg?updatedAt=1738384409932",
+  },
+  {
+    title: "Monorepo: Nx + Storybook in Angular",
+    description:
+      "When you think about the architecture of front-end applications, you have to...",
+    link: "https://medium.com/@diaslalinda/monorepo-nx-storybook-in-angular-7365bd1508ff",
+    image:
+      "https://ik.imagekit.io/dsmblx6kfve/personal-site/article5_cQkXSXKOO.svg?updatedAt=1738384551529",
+  },
+  {
+    title: "Making your Website Accessible to Everyone",
+    description:
+      "If you open a store or a shop, it should serve all customers without any limitations...",
+    link: "https://medium.com/design-bootcamp/making-your-website-accessible-to-everyone-2c702fa8a3c4",
+    image:
+      "https://ik.imagekit.io/dsmblx6kfve/personal-site/article6_-tkyxWp2H.jpg?updatedAt=1738384702113",
+  },
+];
+
+const ArticleItem = ({
+  title,
+  description,
+  link,
+  image,
+}: {
+  title: string;
+  description: string;
+  link: string;
+  image: string;
+}) => (
+  <li className="flex justify-between mb-10 items-center lg:items-start">
+    <div>
+      <h4 className="text-lg font-semibold text-primaryText">{title}</h4>
+      <p className="text-secondaryText">{description}</p>
+      <a
+        className="text-sm font-light text-blue-500 hover:text-blue-800 underline underline-offset-4 cursor-pointer"
+        href={link}
+        rel="noreferrer"
+        target="_blank"
+      >
+        Read the whole story
+      </a>
+    </div>
+    <Image
+      src={image}
+      alt={title}
+      width={80}
+      height={80}
+      className="w-20 h-20 md:w-20 md:h-20 ml-10"
+    />
+  </li>
+);
+
 export default function Articles() {
   return (
     <section>
-      <div
-        className="h-[40rem] bg-fixed bg-center bg-cover"
-        style={{
-          backgroundImage: `url('https://ik.imagekit.io/dsmblx6kfve/personal-site/WhatsApp%20Image%202025-01-29%20at%2010.45.30_IUFXIEUwo.jpeg?updatedAt=1738118841490')`,
-        }}
-      ></div>
-      <div className="h-full pt-12 -mt-[8rem] bg-white lg:w-4/5 2xl:w-3/5 mx-auto px-8">
+      <div className="relative overflow-hidden bg-fixed bg-center bg-cover">
+        <Image
+          src="https://ik.imagekit.io/dsmblx6kfve/personal-site/p1_49_PZjkm49bp.jpg?updatedAt=1738397591448"
+          alt="Parallax Background"
+          width={1400}
+          height={600}
+          quality={100}
+          priority
+          className="absolute top-0 left-0 w-full h-[35rem] object-cover"
+        />
+        <div className="relative z-10 h-full pt-12 mt-[27rem] bg-white lg:w-4/5 2xl:w-3/5 mx-auto px-8">
           <div>
-            <h6 className="text-md text-secondaryText mb-1">
-              I Share
-            </h6>
+            <h6 className="text-md text-secondaryText mb-1">I Share</h6>
             <h2 className="text-5xl font-Playfair_Display font-bold">
               Stories and Learnings
             </h2>
           </div>
           <ul className="mt-16">
-            <li className="flex justify-between mb-10 items-center lg:items-start">
-              <div>
-                <h4 className="text-lg font-semibold text-primaryText">Create Your First Angular Library — Beginner Guide</h4>
-                <p className="text-secondaryText">When our projects become larger, we may have to split them into...</p>
-                <a className="text-sm font-light text-blue-500 hover:text-blue-800 underline underline-offset-4 cursor-pointer" href="https://medium.com/@diaslalinda/create-your-first-angular-library-beginner-guide-4e0d98b13475" rel="noreferrer" target="_blank">Read the whole story</a>
-              </div>
-              <Image src = "https://ik.imagekit.io/dsmblx6kfve/personal-site/Npm-logo_x3S85qlOG.png?updatedAt=1738117954522" alt = "Article 1 Image" width={80} height={80} className="w-20 h-20 md:w-20 md:h-20 ml-10" />
-            </li>
-            <li className="flex justify-between mb-10">
-              <div>
-                <h4 className="text-lg font-semibold text-primaryText">Micro Frontends: Introduction</h4>
-                <p className="text-secondaryText">It’s not late to learn about micro frontends although the concept was first introduced in 2016...</p>
-                <a className="text-sm font-light text-blue-500 hover:text-blue-800 underline underline-offset-4 cursor-pointer" href="https://medium.com/@diaslalinda/micro-frontends-introduction-f51b82c99032" rel="noreferrer" target="_blank">Read the whole story</a>
-              </div>
-              <Image src = "https://ik.imagekit.io/dsmblx6kfve/personal-site/1_aucGHg1tqynhECtJwQAsSQ_7H3iiR7pJ.webp?updatedAt=1738081287532" alt = "Article 1 Image" width={80} height={80} className="w-20 h-20 md:w-20 md:h-20 ml-10" />
-            </li>
-            <li className="flex justify-between mb-10">
-              <div>
-                <h4 className="text-lg font-semibold text-primaryText">Which Text Case to Use</h4>
-                <p className="text-secondaryText">Ever wondered which text case to use ? A little thing like capitalization...</p>
-                <a className="text-sm font-light text-blue-500 hover:text-blue-800 underline underline-offset-4 cursor-pointer" href="https://medium.com/@diaslalinda/which-text-case-to-use-d583b2ad2006" rel="noreferrer" target="_blank">Read the whole story</a>
-              </div>
-              <Image src = "https://ik.imagekit.io/dsmblx6kfve/personal-site/Screenshot%20(2)_yGixKIMMo.png?updatedAt=1738081191939" alt = "Article 1 Image" width={80} height={80} className="w-20 h-20 md:w-20 md:h-20 ml-10" />
-            </li>
-            <li className="flex justify-between mb-10">
-              <div>
-                <h4 className="text-lg font-semibold text-primaryText">Managing Large UI Development Projects</h4>
-                <p className="text-secondaryText">Usually people thinks that CSS or SASS can be written in any way.</p>
-                <a className="text-sm font-light text-blue-500 hover:text-blue-800 underline underline-offset-4 cursor-pointer" href="https://medium.com/@diaslalinda/managing-large-ui-development-projects-61a7166834f7" rel="noreferrer" target="_blank">Read the whole story</a>
-              </div>
-              <Image src = "https://ik.imagekit.io/dsmblx6kfve/personal-site/Screenshot%20(1)_zPnj6vOqw.png?updatedAt=1738080744704" alt = "Article 1 Image" width={80} height={80} className="w-20 h-20 md:w-20 md:h-20 ml-10" />
-            </li>
-            <li className="flex justify-between mb-10">
-              <div>
-                <h4 className="text-lg font-semibold text-primaryText">Monorepo: Nx + Storybook in Angular</h4>
-                <p className="text-secondaryText">When you think about the architecture of front-end applications, you have to...</p>
-                <a className="text-sm font-light text-blue-500 hover:text-blue-800 underline underline-offset-4 cursor-pointer" href="https://medium.com/@diaslalinda/monorepo-nx-storybook-in-angular-7365bd1508ff" rel="noreferrer" target="_blank">Read the whole story</a>
-              </div>
-              <Image src = "https://ik.imagekit.io/dsmblx6kfve/personal-site/image%20(5)_sLmdqe2BQ.png?updatedAt=1738080657803" alt = "Article 1 Image" width={80} height={80} className="w-20 h-20 md:w-18 md:h-18 ml-10" />
-            </li>
-            <li className="flex justify-between mb-10">
-              <div>
-                <h4 className="text-lg font-semibold text-primaryText">Making your Website Accessible to Everyone</h4>
-                <p className="text-secondaryText">If you open a store or a shop, it should serve all customers without any limitations...</p>
-                <a className="text-sm font-light text-blue-500 hover:text-blue-800 underline underline-offset-4 cursor-pointer" href="https://medium.com/design-bootcamp/making-your-website-accessible-to-everyone-2c702fa8a3c4" rel="noreferrer" target="_blank">Read the whole story</a>
-              </div>
-              <Image src = "https://ik.imagekit.io/dsmblx6kfve/personal-site/0_tred27_tDP9T5P6g_ba2e6CP4V.webp?updatedAt=1738080402789" alt = "Article 1 Image" width={80} height={80} className="w-20 h-20 md:w-18 md:h-18 ml-10" />
-            </li>
+            {articles.map((article, index) => (
+              <ArticleItem key={index} {...article} />
+            ))}
           </ul>
+        </div>
       </div>
     </section>
   );
--- src/components/casestudies/index.tsx
+++ src/components/casestudies/index.tsx
@@ -12,98 +12,126 @@ export default function CaseStudies() {
         </h2>
       </div>
       <div className="mt-20">
-        <div className="mt-20 md:mt-0">
-          <a
-            href="https://www.behance.net/gallery/131223179/RouteSONAR-Route-Optimization-Web-Application"
-            className="md:flex mt-20"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <Image
-              src="https://ik.imagekit.io/dsmblx6kfve/personal-site/banner1_OdQx1_JQs.jpg?updatedAt=1737901498046"
-              alt="Banner 1 Image"
-              width={520}
-              height={520}
-              className="md:pr-2 md:w-1/2 transition-transform duration-300 hover:scale-110"
-            />
-            <div className="md:w-1/2 md:pl-8 pt-4 md:pt-0">
-              <h6 className="text-2xl font-bold mb-3">RouteSONAR</h6>
-              <p className="text-md text-secondaryText font-light">
-                RouteSONAR is an innovative route optimization platform
-                developed to find the most efficient travel route out of all
-                possible outcomes mapped across the multiple last mile delivery
-                locations.
-              </p>
-              <button className="my-5 text-lg text-buttonText">
-                View Project --------&gt;
-              </button>
-              <div className="grid grid-cols-2 gap-4 mt-3 md:mt-12">
-                <div>
-                  <span className="mb-2">Roles</span>
-                  <ul>
-                    <li className="text-sm text-list font-light leading-6">Frontend Developer</li>
-                    <li className="text-sm text-list font-light leading-6">
-                      Lo-fi & Hi-fi prototyping
-                    </li>
-                    <li className="text-sm text-list font-light leading-6">UI/UX</li>
-                  </ul>
-                </div>
-                <div>
-                  <span className="mb-2">Tools</span>
-                  <ul>
-                    <li className="text-sm text-list font-light leading-6">React JS</li>
-                    <li className="text-sm text-list font-light leading-6">Figma</li>
-                  </ul>
-                </div>
+        <div className="md:mt-0 md:flex">
+          <Image
+            src="https://ik.imagekit.io/dsmblx6kfve/personal-site/Banner-1_GkmxtYtTD.jpg?updatedAt=1738245948245"
+            alt="Banner 1 Image"
+            width={520}
+            height={520}
+            className="md:pr-2 md:w-1/2 transition-transform duration-300 hover:scale-110 object-cover"
+            quality={100}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 800px"
+          />
+          <div className="md:w-1/2 md:pl-8 pt-4 md:pt-0">
+            <h6 className="text-2xl font-bold mb-3">RouteSONAR</h6>
+            <p className="text-md text-secondaryText font-light">
+              RouteSONAR is an innovative route optimization platform developed
+              to find the most efficient travel route out of all possible
+              outcomes mapped across the multiple last mile delivery locations.
+            </p>
+            <a
+              href="https://www.behance.net/gallery/131223179/RouteSONAR-Route-Optimization-Web-Application"
+              className="md:flex mt-20 my-5 text-lg text-buttonText flex items-center"
+              rel="noreferrer"
+              target="_blank"
+            >
+              View Project{" "}
+              <Image
+                src="https://ik.imagekit.io/dsmblx6kfve/personal-site/view.face245a_rdVEXw_Ee.svg?updatedAt=1738382591186"
+                alt="Forward button image"
+                width={80}
+                height={20}
+                className="ml-5"
+              />
+            </a>
+            <div className="grid grid-cols-2 gap-4 mt-3 md:mt-12">
+              <div>
+                <span className="mb-2">Roles</span>
+                <ul>
+                  <li className="text-sm text-list font-light leading-6">
+                    Frontend Developer
+                  </li>
+                  <li className="text-sm text-list font-light leading-6">
+                    Lo-fi & Hi-fi prototyping
+                  </li>
+                  <li className="text-sm text-list font-light leading-6">
+                    UI/UX
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <span className="mb-2">Tools</span>
+                <ul>
+                  <li className="text-sm text-list font-light leading-6">
+                    React JS
+                  </li>
+                  <li className="text-sm text-list font-light leading-6">
+                    Figma
+                  </li>
+                </ul>
               </div>
             </div>
-          </a>
+          </div>
         </div>
-        <div className="mt-20 md:mt-0">
-          <a
-            href="https://www.behance.net/gallery/121582261/Camp-Search-mobile-app"
-            className="md:flex mt-20"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <Image
-              src="https://ik.imagekit.io/dsmblx6kfve/personal-site/banner2_m7lmFAu2f.jpg?updatedAt=1737938970416"
-              alt="Banner 1 Image"
-              width={520}
-              height={520}
-              className="md:pr-2 md:w-1/2 transition-transform duration-300 hover:scale-110"
-            />
-            <div className="md:w-1/2 md:pl-8 pt-4 md:pt-0">
-              <h6 className="text-2xl font-bold mb-3">CampMo</h6>
-              <p className="text-md text-secondaryText font-light">
-                The platform that allows travellers to find their ideal
-                campgrounds for their holidays and get recommendations based on
-                their preferences.
-              </p>
-              <button className="my-5 text-lg text-buttonText">
-                View Project --------&gt;
-              </button>
-              <div className="grid grid-cols-2 gap-4 mt-3 md:mt-12">
-                <div>
-                  <span className="mb-2">Roles</span>
-                  <ul>
-                    <li className="text-sm text-list font-light leading-6">
-                      Lo-fi & Hi-fi prototyping
-                    </li>
-                    <li className="text-sm text-list font-light leading-6">UI/UX Lead</li>
-                    <li className="text-sm text-list font-light leading-6">UI/UX</li>
-                  </ul>
-                </div>
-                <div>
-                  <span className="mb-2">Tools</span>
-                  <ul>
-                    <li className="text-sm text-list font-light leading-6">Figma</li>
-                    <li className="text-sm text-list font-light leading-6">Protopie</li>
-                  </ul>
-                </div>
+        <div className="mt-20 md:mt-0 md:flex pt-20">
+          <Image
+            src="https://ik.imagekit.io/dsmblx6kfve/personal-site/Banner-2_YR4VE_kGz.jpg?updatedAt=1738247020705"
+            alt="Banner 1 Image"
+            width={520}
+            height={520}
+            className="md:pr-2 md:w-1/2 transition-transform duration-300 hover:scale-110 object-cover"
+            quality={100}
+          />
+          <div className="md:w-1/2 md:pl-8 pt-4 md:pt-0">
+            <h6 className="text-2xl font-bold mb-3">CampMo</h6>
+            <p className="text-md text-secondaryText font-light">
+              The platform that allows travellers to find their ideal
+              campgrounds for their holidays and get recommendations based on
+              their preferences.
+            </p>
+            <a
+              href="https://www.behance.net/gallery/121582261/Camp-Search-mobile-app"
+              className="md:flex mt-20 my-5 text-lg text-buttonText flex items-center"
+              rel="noreferrer"
+              target="_blank"
+            >
+              View Project{" "}
+              <Image
+                src="https://ik.imagekit.io/dsmblx6kfve/personal-site/view.face245a_rdVEXw_Ee.svg?updatedAt=1738382591186"
+                alt="Forward button image"
+                width={80}
+                height={20}
+                className="ml-5"
+              />
+            </a>
+            <div className="grid grid-cols-2 gap-4 mt-3 md:mt-12">
+              <div>
+                <span className="mb-2">Roles</span>
+                <ul>
+                  <li className="text-sm text-list font-light leading-6">
+                    Lo-fi & Hi-fi prototyping
+                  </li>
+                  <li className="text-sm text-list font-light leading-6">
+                    UI/UX Lead
+                  </li>
+                  <li className="text-sm text-list font-light leading-6">
+                    UI/UX
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <span className="mb-2">Tools</span>
+                <ul>
+                  <li className="text-sm text-list font-light leading-6">
+                    Figma
+                  </li>
+                  <li className="text-sm text-list font-light leading-6">
+                    Protopie
+                  </li>
+                </ul>
               </div>
             </div>
-          </a>
+          </div>
         </div>
       </div>
     </section>
